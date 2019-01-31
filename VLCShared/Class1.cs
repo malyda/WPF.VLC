a1cc9e38f085f31d9a20204b45379e728423a64c
David Malý <malyce.dvd@gmail.com>
2019-01-31 16:05:29 +0100

    Add project files.

---
+++ VLCShared/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace VLCShared
+{
+    public class Class1
+    {
+    }
+}
---
+++ WPF.VLC/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Path = System.IO.Path;
+
+namespace WPF.VLC
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var currentAssembly = Assembly.GetEntryAssembly();
+            var currentDirectory = new FileInfo(currentAssembly.Location).DirectoryName;
+            // Default installation path of VideoLAN.LibVLC.Windows
+            var libDirectory = new DirectoryInfo(Path.Combine(currentDirectory, "libvlc", IntPtr.Size == 4 ? "win-x86" : "win-x64"));
+            vlcPlayer.MediaPlayer.VlcLibDirectory = libDirectory;
+            vlcPlayer.MediaPlayer.EndInit();
+            vlcPlayer.MediaPlayer.Play(new Uri("http://download.blender.org/peach/" +
+                "bigbuckbunny_movies/big_buck_bunny_480p_surround-fix.avi"));
+        }
+    }
+}
